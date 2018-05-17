a4f7228a8e678b8f9aa5162696b8ac7608d81dc9
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-05-17 12:43:02 +0200

    example file

--- src/example.js
+++ src/example.js
@@ -1,9 +1,10 @@
-import {Builder} from "selenium-webdriver";
-
+"use strict";
+exports.__esModule = true;
+/*
 (async () =>{
     const driver = await new Builder().forBrowser('chrome').build();
 
     await driver.get('https://www.google.com/')
         .then(_=>driver.sleep(5000));
     await driver.quit();
-})();
+})();*/
